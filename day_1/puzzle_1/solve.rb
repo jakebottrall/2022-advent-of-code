814ef81cfce6228fd951fccdf850409729a01360
jakebottrall <jakebottrall@gmail.com>
2023-08-25 16:26:18 -0700

    use min instead of sort + first

--- day_1/puzzle_1/solve.rb
+++ day_1/puzzle_1/solve.rb
@@ -5,9 +5,11 @@ file_path     = File.join(__dir__, 'input.txt')
 
 File.open(file_path, 'r') do |file|
   elves = file.read.split("\n\n")
-  calories = elves.map { |elf| elf.split("\n").inject(0) { |sum, cal| sum + cal.to_i } }
-  sorted_calories = calories.sort { |a, b| b - a }
 
-  puts("Answer: #{sorted_calories.first}")
+  calories = elves
+             .map { |elf| elf.split("\n").inject(0) { |sum, cal| sum + cal.to_i } }
+             .min { |a, b| b - a }
+
+  puts("Answer: #{calories}")
   puts("Solved in: #{(Time.now - start_at) * 1000}ms")
 end
