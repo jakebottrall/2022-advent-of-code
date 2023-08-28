e5af92ef6535fb60e9059113357889262e8320c9
jakebottrall <jakebottrall@gmail.com>
2023-08-27 17:41:10 -0700

    add benchmarking for python

---
+++ benchmark/benchmark.py
@@ -0,0 +1,40 @@
+import math
+import time
+from typing import List
+
+ITERATIONS = 10000
+
+
+def benchmark(callback):
+    run_times: List[str] = []
+
+    answer = 0
+
+    for x in range(ITERATIONS):
+        start_at = time.time()
+        answer = callback()
+        end_at = time.time()
+        run_times.append((end_at - start_at) * 1000)
+
+    run_times.sort()
+
+    total = sum(run_times)
+    average = total / ITERATIONS
+    median = run_times[math.floor(ITERATIONS / 2)]
+    p95 = run_times[math.floor(ITERATIONS * 0.95)]
+    p99 = run_times[math.floor(ITERATIONS * 0.99)]
+
+    print(
+        {
+            "answer": answer,
+            "total": parse_result(total),
+            "average": parse_result(average),
+            "median": parse_result(median),
+            "p95": parse_result(p95),
+            "p99": parse_result(p99),
+        }
+    )
+
+
+def parse_result(result: int):
+    return "{:.5f}ms".format(result)
--- day_1/puzzle_1/solve.py
+++ day_1/puzzle_1/solve.py
@@ -1,6 +1,8 @@
 import os
 import time
 
+from benchmark import benchmark
+
 
 def get_file_path():
     directory = os.path.dirname(os.path.abspath(__file__))
@@ -12,19 +14,14 @@ def calculate_total(elf: str):
 
 
 def main():
-    start_at = time.time()
     file_path = get_file_path()
 
     with open(file_path, "r") as file:
         elves = file.read().split("\n\n")
         calories = [calculate_total(elf) for elf in elves]
         sorted_calories = sorted(calories, reverse=True)
-
-        end_at = time.time()
-
-        print(f"Answer: {sorted_calories[0]}")
-        print(f"Solved in: {(end_at - start_at) * 1000}ms")
+        return sorted_calories[0]
 
 
 if __name__ == "__main__":
-    main()
+    benchmark(main)
